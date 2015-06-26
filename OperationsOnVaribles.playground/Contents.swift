99064eeeba836da258bf86c6b9bbb6e4159293cb
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-25 19:49:29 -0700

    Operations on Variables Ints

---
+++ OperationsOnVaribles.playground/Contents.swift
@@ -0,0 +1,11 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var x = 49
+var y = 10
+
+var addtionAnswer = x + y
+var multiplicationAnswer = x * y
+var subtractionAnswer = x - y
+var divisionAnswer = x / y
--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -2,4 +2,22 @@
 
 import UIKit
 
-var firstInteger = 0
+var firstInteger:Int = 0, secondInteger:Int = 29
+var thirdInteger:Double = 1000858
+var x:Int = 5
+
+// I will be using this later to track the discounts at this department store - it will change seasonaly
+var discountAtDepartmentStore = 0.3
+
+
+// first line
+// second line
+// UITableViewDataSource
+
+/*
+
+paragrgh1 : Explanation
+
+paragrapgh2 : Explanation
+
+*/
