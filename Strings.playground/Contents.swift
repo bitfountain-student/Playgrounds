57f90e23db7ca67c9f377273cee74cf4e54eab44
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-25 23:51:25 -0700

    completed:  Strings and Characters

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var string1 = ""
+string1 = "Hello"
+var string2 = "World"
+var helloWorldString = string1 + " " + string2
+helloWorldString = helloWorldString.uppercaseString
+"hello world".uppercaseString
+
+var firstCharacter:String = "!"
+helloWorldString = helloWorldString + firstCharacter
+
+var z=9
+
+
