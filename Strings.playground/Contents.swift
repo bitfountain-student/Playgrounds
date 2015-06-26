1b60379426364f595e85c678fa3d02c79f6a1dc1
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-26 00:21:00 -0700

    completed: Optionals Int

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -12,6 +12,12 @@ helloWorldString = helloWorldString.uppercaseString
 var firstCharacter:String = "!"
 helloWorldString = helloWorldString + firstCharacter
 
-var z=9
+var x=5
+var newString = "\(x) times " + helloWorldString
+var doubleValue = 3.5
+var newDoubleString = "\(doubleValue)" + "1"
 
-
+var numberString = "9"
+var numberStringToInt = numberString.toInt()
+var optionalToInt = numberStringToInt!
+optionalToInt = optionalToInt + 3
