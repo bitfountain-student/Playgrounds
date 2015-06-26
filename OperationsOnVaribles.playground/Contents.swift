2050ae9c39d120c170ab6e2887183288f7a29848
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-25 20:25:41 -0700

    completed: Typealias

--- OperationsOnVaribles.playground/Contents.swift
+++ OperationsOnVaribles.playground/Contents.swift
@@ -1,8 +1,10 @@
 // Playground - noun: a place where people can play
 
 import UIKit
+typealias WholeNumber = Int
+typealias NumberWithDecimal = Double
 
-var x = 49
+var x:WholeNumber = 49
 var y = 10
 
 var addtionAnswer = x + y
@@ -11,7 +13,11 @@ var subtractionAnswer = x - y
 var divisionAnswer = x / y
 
 var discountOnShoesAtStore = 0.3
-var priceOfShoes:Double = 33
+var priceOfShoes:NumberWithDecimal = 33
 var priceAfterDiscount = priceOfShoes * (1.0 - discountOnShoesAtStore)
 var downPayment = 10.5
 priceAfterDiscount = priceAfterDiscount - downPayment
+
+
+var z = 33
+z = 44
