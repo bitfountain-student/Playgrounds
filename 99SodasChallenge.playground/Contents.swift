3baa307fdb0b687dd00d11f6a916dc42b81822f3
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-28 15:16:27 -0700

    completed: 99 Sodas Challenge

---
+++ 99SodasChallenge.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+for var numberOfBottlesOnTheWall = 99; numberOfBottlesOnTheWall >= 0; numberOfBottlesOnTheWall--
+{
+  println("\(numberOfBottlesOnTheWall) bottles on the wall")
+
+
+
+}
