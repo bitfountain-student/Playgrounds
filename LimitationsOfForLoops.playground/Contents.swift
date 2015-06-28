abbf1749b53c901fa9a2a5683e7001821d791694
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-28 15:04:21 -0700

    Limitations of For Loops

---
+++ LimitationsOfForLoops.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var wheat:Int = 1
+for var i = 0; i < 94; ++i {
+  wheat = wheat * 2
+  println("location on board \(i) and pieces of wheat: \(wheat)")
+}
