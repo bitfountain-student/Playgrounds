1a7b5544f20f980fc63770ebfd22b4a6b2839d60
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-28 14:43:48 -0700

    completed; For Loops

---
+++ ForLoops.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+//import UIKit
+//
+//var meditationHours = 1
+//
+//for meditationHours; meditationHours < 100; meditationHours = meditationHours + 1 {
+//  println("I am getting more enlightened")
+//}
+
+for var meditationHours = 100; meditationHours > 1; meditationHours-- {
+  println("I am gettin more and more enlightened")
+}
