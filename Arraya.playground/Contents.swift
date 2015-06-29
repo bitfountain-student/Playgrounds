a221669da8dc58be2774ec0fc33688c0af76bffe
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-29 11:48:34 -0700

    Completed: Create an Array

---
+++ Arraya.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//var tigerNames:Array<String>
+//var tigerNames:[String]
+var tigerNames = ["Tigger", "Tigress", "Jacob", "Spar"]
+var tigerAges = [3, 2, 4, 5]
