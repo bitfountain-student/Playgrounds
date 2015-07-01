af6e1b47988ec7c76a091775aaeeee4f10a7897b
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-30 18:09:09 -0700

    completed: Our First Function

---
+++ Functions.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func printHelloWorld(){
+  println ("Hello, World!")
+    println("Hello, Class")
+  }
+
+  printHelloWorld()
+  printHelloWorld()
+  printHelloWorld()
