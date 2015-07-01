924f2898609431170ae73e19a52658653a45992a
bitfountain-student <bitfountain-student@slowchat.us>
2015-06-30 18:51:54 -0700

    completed: Another Return Value and Other Function Calls

--- Functions.playground/Contents.swift
+++ Functions.playground/Contents.swift
@@ -4,17 +4,14 @@ import UIKit
 
 func printHelloWorld(){
   println ("Hello, World!")
-    println("Hello, Class")
-  }
+  println("Hello, Class")
+}
 
 func printNumberSupplied (number: Int) {
   println("The number is \(number)")
   println("The number times three is \(number * 3)")
   var numberTimesTwo = number * 2
   numberTimesTwo *= 2
-  
-  
-  
 }
 
 func turnOffAppliance (applianceName: String, isOn: Bool) {
@@ -34,5 +31,11 @@ func additionFunction(firstParameter:Int, secondParameter:Int) -> Int {
 
 var additionAnswerOfInteger = additionFunction(2, 4)
 let finalAnswer = additionFunction(additionAnswerOfInteger, 5)
-println(finalAnswer)
 
+func helloWorldString() -> String {
+  printHelloWorld()
+  return "Hello World"
+}
+
+var helloWorld = helloWorldString() + "!"
+println(helloWorld)
