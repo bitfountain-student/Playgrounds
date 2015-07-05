f4b2ef621bd20c98aa84b8ec71f1fa4f60aebd98
bitfountain-student <bitfountain-student@slowchat.us>
2015-07-04 18:14:29 -0700

    Introduction to Tuples

---
+++ Tuples.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var myTuple = ("Tigger", "Bengal", 3)
+let name = myTuple.0
+
+var secondTigerTuple = (name: "Tigress", breed: "Indochinese", age: 2)
+
+let age = secondTigerTuple.age
