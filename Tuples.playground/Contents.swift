5ca2ba3ff351b237c108bcbafc2a7704bf4c81c1
bitfountain-student <bitfountain-student@slowchat.us>
2015-07-04 19:11:20 -0700

    complted: Switch Statement with a Tuple

--- Tuples.playground/Contents.swift
+++ Tuples.playground/Contents.swift
@@ -6,6 +6,15 @@ import UIKit
 var myTuple = ("Tigger", "Bengal", 3)
 let name = myTuple.0
 
-var secondTigerTuple = (name: "Tigress", breed: "Indochinese", age: 2)
+var secondTigerTuple = (name: "Tigress", breed: "Himilayan", age: 2)
 
 let age = secondTigerTuple.age
+
+switch secondTigerTuple {
+case ("Tigress", "Himilayan", 2):
+  println("first case is true")
+case (_, "Indochinese", _):
+  println("Second case is true")
+default:
+  println("Defalt case is  occuring")
+}
